74f0f37da8ae9190cfc8cf897c8cca73f73ac807
papayetoo <rhkdgus0826@gmail.com>
2020-09-05 00:10:54 +0900

    9.4 커밋 9.4 monthly problem partition_labels.py

---
+++ LeetCode/partition_labels.py
@@ -0,0 +1,33 @@
+from typing import List
+class Solution:
+    def partitionLabels(self, s:str) -> List[int]:
+        last = {}
+        for index, char in enumerate(s):
+            # for saving last index of character
+            last[char] = index
+
+        def findLastIndex(sub: str, st: int, end:int):
+            nMax = 0
+            for char in sub:
+                nMax = max(nMax, last[char])
+            if nMax == end:
+                return nMax
+            else:
+                return findLastIndex(s[st:nMax+1],set(sub[:nMax+1]), st, nMax)
+
+        i = 0
+        ans = []
+        while i < len(s):
+            end = last[s[i]]
+            sub = s[i:end+1]
+            charSet = set(sub)
+            end = findLastIndex(sub, charSet, i, end)
+            ans.append(len(s[i:end+1]))
+            i = end + 1
+
+        return ans
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.partitionLabels('ababcbacadefegdehijhklij'))
