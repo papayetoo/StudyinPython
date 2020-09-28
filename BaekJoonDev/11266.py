f2c92d84e7bd11670b221cdf8680bd6ea0e3fdf6
papayetoo <rhkdgus0826@gmail.com>
2020-09-28 22:42:38 +0900

    단절점 알고리즘 (Articulation Point algorithm) 풀이 백준 11266

--- BaekJoonDev/11266.py
+++ BaekJoonDev/11266.py
@@ -9,7 +9,7 @@ class Solution:
         self.dfn = [0] * (nVertex + 1)
         self.low = [float('inf')] * (nVertex + 1)
         self.count = 1
-        self.answer = []
+        self.answer = set()
 
     def dfs(self, adj: defaultdict, visited: list, u: int):
         visited.append(u)
@@ -19,26 +19,25 @@ class Solution:
             if v not in visited:
                 self.count += 1
                 self.dfs(adj, visited, v)
+                if self.dfn[u] <= self.low[v]:
+                    self.answer.add(u)
+                self.low[u] = min(self.low[u], self.low[v])
+            else:
+                self.low[u] = min(self.low[u], self.dfn[v])
 
-    def dfslow(self, adj: defaultdict, parent: int, child: int):
-        for next in adj[child]:
-            if next != parent:
-                self.low[next] = min(self.low[next], self.low[child])
-            # self.low[next] = min(self.low[next], self.low[child])
-
-    def findConnectedComponent(self, adj: defaultdict, node: int, isRoot: bool):
-        if isRoot and len(adjList[node]) >= 2:
-            self.answer.append(node)
-            return
-        elif not isRoot:
-            # 늦은 dfs 탐색번호 갖는 자식노드만 추출.
-            lateNodes = [x for x in adj[node] if self.dfn[node] < self.dfn[x]]
-            for i in lateNodes:
-                if self.dfn[node] <= self.low[i]:
-                    self.answer.append(node)
-                    return
-                else:
-                    continue
+    # def findConnectedComponent(self, adj: defaultdict, node: int, isRoot: bool):
+    #     if isRoot and len(adjList[node]) >= 2:
+    #         self.answer.append(node)
+    #         return
+    #     elif not isRoot:
+    #         # 늦은 dfs 탐색번호 갖는 자식노드만 추출.
+    #         lateNodes = [x for x in adj[node] if self.dfn[node] < self.dfn[x]]
+    #         for i in lateNodes:
+    #             if self.dfn[node] <= self.low[i]:
+    #                 self.answer.append(node)
+    #                 return
+    #             else:
+    #                 continue
 
 
 if __name__ == '__main__':
@@ -50,11 +49,7 @@ if __name__ == '__main__':
         adjList[v].append(u)
     s = Solution(numOfVertex)
     s.dfs(adjList, [], 1)
-    for i in range(1, numOfVertex + 1):
-        for child in adjList[i]:
-            s.dfslow(adjList, i, child)
-        # s.findConnectedComponent(adjList, i, True if i == 1 else False)
-    print('dfn', s.dfn)
-    print('low', s.low)
-    print(len(s.answer))
-    print(*s.answer)
+    answer = list(s.answer)
+    answer.sort()
+    print(len(answer))
+    print(*answer)
